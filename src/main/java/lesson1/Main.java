ae87b7066dbb2af945883bfa5aa3d9185746e58e
Vitalii Sednev <phen1007@gmail.com>
2021-07-07 15:22:08 +0400

    modify add resources folder at main

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
